e03989b28d142ae7d378156c4ee79644c3a6b025
BeatrizOSantos <beatrizodsk@gmail.com>
2022-05-02 15:33:52 -0300

    feat: basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { HomeComponent } from './features/home/home.component';
+import { AboutMeComponent } from './features/about-me/about-me.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { JourneyComponent } from './features/journey/journey.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, JourneyComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
